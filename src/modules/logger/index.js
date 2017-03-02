1b324e50ad1f494c8cebc517e403f6eae01ef68b
Nazar Kuzmenko <nazar@streamcrease.com>
2017-03-02 05:43:17 +0200

    Basic logger

--- src/modules/logger/index.js
+++ src/modules/logger/index.js
@@ -1,1 +1,17 @@
 const winston = require('winston')
+const config = require('config')
+
+const logger = new winston.Logger({
+  level: config.get('logs.level'),
+  transports: [
+    new winston.transports.Console({ colorize: true }),
+  ],
+})
+
+logger.stream = {
+  write (message) {
+    logger.info(message)
+  },
+}
+
+module.exports = logger
--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const passport = require('passport')
 const mongoose = require('mongoose')
 const helmet = require('helmet')
 const session = require('express-session')
+const morgan = require('morgan')
 
 mongoose.Promise = require('bluebird')
 
 const router = require('./routes')
+const log = require('./modules/logger')
 const errorHandler = require('./middlewares/error.handler')
 
 const app = express()
@@ -16,11 +18,12 @@ app.use(helmet())
 app.use(session(config.get('session')))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(morgan('tiny', { stream: log.stream }))
 app.use(router)
 app.use(errorHandler)
 
 mongoose.connect(config.get('database.mongodb.uri'))
 
 app.listen(config.get('server.port'), () => {
-  console.log(`Server is running on ${config.get('server.port')} port`)
+  log.info(`Server is running on ${config.get('server.port')} port`)
 })
